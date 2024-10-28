d2be3c56036edeb0dc7dde67682c81e36e9c07c7
raj-singhh <135631160+raj-singhh@users.noreply.github.com>
2024-10-28 08:00:57 +0530

    Update script.js

--- SpotifyClone/js/script.js
+++ SpotifyClone/js/script.js
@@ -18,7 +18,7 @@ function secondsToMinutesSeconds(seconds) {
 
 async function getSongs(folder) {
     currfolder = folder;
-    let a = await fetch(`/${folder}/`);
+    let a = await fetch(`/${folder}/`);//if you are using vs code than attach the live server address to it ex-(`http://127.0.0.1:34532/${folder}`)
     let response = await a.text();
 
     let div = document.createElement("div");
@@ -68,7 +68,7 @@ const playMusic = (track, pause = false) => {
 }
 async function displayAlbums() {
     console.log("displaying albums")
-    let a = await fetch(`/songs/`)
+    let a = await fetch(`/songs/`)//if you are using vs code than attach the live server address to it ex-(`http://127.0.0.1:51844/songs/`)
     let response = await a.text();
     let div = document.createElement("div");
     div.innerHTML = response;
@@ -81,7 +81,7 @@ async function displayAlbums() {
         if (e.href.includes("/songs/")) {
             let folder = e.href.split("/").slice(-1)[0];
             //Get the metadata of the folder
-            let a = await fetch(`/songs/${folder}/info.json`);
+            let a = await fetch(`/songs/${folder}/info.json`);//if you are using vs code than attach the live server address to it ex(`http://127.0.0.1:51844/songs/${folder}/info.json`)
 
             let response = await a.json();
             cardContainer.innerHTML =cardContainer.innerHTML +  ` <div data-folder="${folder}" class="card">
